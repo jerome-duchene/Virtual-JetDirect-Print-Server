9eec3a2512bf451e00a576230cb6f7db17e45d10
phozen <jerome@phozen.be>
2020-07-09 14:51:27 +0200

    Update assembly version (forgotted for the last commit)

--- UnitTesting/Properties/AssemblyInfo.cs
+++ UnitTesting/Properties/AssemblyInfo.cs
@@ -16,5 +16,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("3a23140f-5030-43ce-ae07-1b94103eebcb")]
 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
